ee079e58902d3380faaffdd9f8a7ef6ad2ca5c99
Shohjahon <Kamondor0707@gmail.com>
2025-09-13 08:07:57 +0500

    Optimization

--- amaliyotlar/lesson4/lesson4.2.6.py
+++ amaliyotlar/lesson4/lesson4.2.6.py
@@ -4,4 +4,4 @@ if login_admin == foydalanuvchi_ismi:
     print('Xush kelibsiz, Admin. Foydalanuvchilar ro\'yxatini ko\'rasizmi?')
 else:
     print(f"Xush kelibsiz, {foydalanuvchi_ismi}")
-# CYBERSPACE.UZ: CTF: Fake{https://t.me/+JV57FHn4BCpkNjQy}
+# hidden url https://t.me/+JV57FHn4BCpkNjQy
